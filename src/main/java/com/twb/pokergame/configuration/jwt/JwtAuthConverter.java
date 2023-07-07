63d3ed7c09b58d60114f0e6f0924d3d55a7f1397
Bigger, Thomas <thomasbigger584@gmail.com>
2023-07-07 13:23:39 +0100

    spring authenticating with keycloak

--- src/main/java/com/twb/pokergame/configuration/jwt/JwtAuthConverter.java
+++ src/main/java/com/twb/pokergame/configuration/jwt/JwtAuthConverter.java
@@ -12,22 +12,27 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    private static final String RESOURCE_ACCESS = "resource_access";
+    private static final String ROLES = "roles";
+    private static final String AUTHORITY_PREFIX = "ROLE_";
+
     private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter;
     private final JwtAuthConverterProperties properties;
 
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
-        Collection<GrantedAuthority> authorities = Stream.concat(
+        List<GrantedAuthority> authorities = Stream.concat(
                 jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
-                extractResourceRoles(jwt).stream()).collect(Collectors.toSet());
+                extractResourceRoles(jwt).stream()).collect(Collectors.toList());
 
         return new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt));
     }
@@ -40,17 +45,22 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
         return jwt.getClaim(claimName);
     }
 
-    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
-        Map<String, Object> resource;
-        Collection<String> resourceRoles;
-        if (resourceAccess == null
-                || (resource = (Map<String, Object>) resourceAccess.get(properties.getResourceId())) == null
-                || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
-            return Set.of();
+    @SuppressWarnings("unchecked")
+    private List<SimpleGrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim(RESOURCE_ACCESS);
+        if (!resourceAccess.containsKey(properties.getResourceId())) {
+            return Collections.emptyList();
+        }
+        Map<String, Object> resource = (Map<String, Object>) resourceAccess.get(properties.getResourceId());
+        if (!resource.containsKey("roles")) {
+            return Collections.emptyList();
+        }
+        Collection<String> resourceRoles = (Collection<String>) resource.get(ROLES);
+        if (resourceRoles == null) {
+            return Collections.emptyList();
         }
         return resourceRoles.stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                .collect(Collectors.toSet());
+                .map(role -> new SimpleGrantedAuthority(AUTHORITY_PREFIX + role))
+                .collect(Collectors.toList());
     }
 }
