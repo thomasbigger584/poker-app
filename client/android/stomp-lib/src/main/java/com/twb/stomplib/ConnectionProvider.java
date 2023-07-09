53d7d1b0ea8d94a21c7cc5038377a166f87d15cb
Bigger, Thomas <thomasbigger584@gmail.com>
2023-07-09 14:19:42 +0100

    Adding stomp lib in android

---
+++ client/android/stomp-lib/src/main/java/com/twb/stomplib/ConnectionProvider.java
@@ -0,0 +1,32 @@
+package com.twb.stomplib;
+
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+
+public interface ConnectionProvider {
+
+    /**
+     * Subscribe this for receive stomp messages
+     */
+    Observable<String> messages();
+
+    /**
+     * Sending stomp messages via you ConnectionProvider.
+     * onError if not connected or error detected will be called, or onCompleted id sending started
+     * TODO: send messages with ACK
+     */
+    Completable send(String stompMessage);
+
+    /**
+     * Subscribe this for receive #LifecycleEvent events
+     */
+    Observable<LifecycleEvent> lifecycle();
+
+    /**
+     * Disconnects from server. This is basically a Callable.
+     * Automatically emits Lifecycle.CLOSE
+     */
+    Completable disconnect();
+
+    Completable setHeartbeat(int ms);
+}
