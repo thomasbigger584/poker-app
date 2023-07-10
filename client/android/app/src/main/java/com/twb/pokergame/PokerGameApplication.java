8024f0aba8412f780c57e1bb7c74398bf3e48deb
Bigger, Thomas <thomasbigger584@gmail.com>
2023-07-10 13:58:55 +0100

    Adding hilt to android

---
+++ client/android/app/src/main/java/com/twb/pokergame/PokerGameApplication.java
@@ -0,0 +1,16 @@
+package com.twb.pokergame;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class PokerGameApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
