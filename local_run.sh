a8f6d5e92339d07afd68d135aab684111b15a7e2
Bigger, Thomas <thomasbigger584@gmail.com>
2023-07-12 18:12:40 +0100

    docker server catching ctrl-c and calling rm

--- local_run.sh
+++ local_run.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+trap ctrl_c INT # trap ctrl-c and call ctrl_c()
+
+function ctrl_c() {
+       docker-compose -f server/docker-compose.yml rm --force
+}
 
 docker-compose -f server/docker-compose.yml up --build --remove-orphans
