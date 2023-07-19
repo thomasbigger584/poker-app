48af7800aaa8c9c0867f7580176781b6bb249673
Bigger, Thomas <thomasbigger584@gmail.com>
2023-07-19 14:50:11 +0100

    running app after install in script

--- android_run.sh
+++ android_run.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 cd client/android || exit
-#./gradlew clean build || exit
+./gradlew clean build || exit
 
 adb devices -l | {
   read -r _ # discard first line
   while IFS= read -r line; do
     device_id=$(echo "$line" | awk '{print $1}')
-    if [ -n "$device_id" ]
-    then
+
+    if [ -n "$device_id" ]; then
       device_name=$(echo "$line" | awk '{print $5}')
+
       echo "Installing to $device_name..."
       adb -s $device_id install -r app/build/outputs/apk/debug/app-debug.apk
 
-#       todo: launch app activity
+      echo "Running app on $device_name..."
+      adb -s $device_id shell monkey -p com.twb.pokergame 1
     fi
   done
+
+  echo "done"
 }
