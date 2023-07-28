c3640eacf80a6f1469b8dd15b8ed52e507ecfd81
Bigger, Thomas <thomasbigger584@gmail.com>
2023-07-28 13:40:14 +0100

    removing volume and update remote script

--- remote_run.sh
+++ remote_run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-git pull --recursive
+git checkout master
+
+git pull origin master
+git submodule update --recursive --remote
 
 docker compose -f server/digitalocean/docker-compose.yml up --build --remove-orphans -d
