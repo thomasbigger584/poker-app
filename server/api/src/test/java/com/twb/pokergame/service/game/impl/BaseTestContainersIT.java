5c49857ebad77b4a563a9528097e9ee3f507159b
Bigger, Thomas <thomasbigger584@gmail.com>
2023-08-05 19:00:04 +0100

    working communication between test and endpoints

--- server/api/src/test/java/com/twb/pokergame/service/game/impl/BaseTestContainersIT.java
+++ server/api/src/test/java/com/twb/pokergame/service/game/impl/BaseTestContainersIT.java
@@ -1,5 +1,10 @@
 package com.twb.pokergame.service.game.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.slf4j.Logger;
@@ -8,13 +13,20 @@ import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 
 import java.io.File;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public abstract class BaseTestContainersIT {
+    protected static final HttpClient HTTP_CLIENT = HttpClient.newBuilder().build();
     private static final Logger logger = LoggerFactory.getLogger("TEST");
     private static final String DOCKER_COMPOSE_LOCATION = "src/test/resources/";
     private static final String TEST_DOCKER_COMPOSE_YML = "test-docker-compose.yml";
     private static final String EXPOSED_SERVICE = "api";
     private static final int EXPOSED_PORT = 8081;
+    protected static final String API_BASE_URL = String.format("http://localhost:%d", EXPOSED_PORT);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static DockerComposeContainer<?> dockerComposeContainer;
 
     @BeforeAll
@@ -30,5 +42,48 @@ public abstract class BaseTestContainersIT {
     @AfterAll
     public static void afterAll() {
         dockerComposeContainer.stop();
+    }
+
+    protected static <ResultBody, RequestBody> ApiHttpResponse<ResultBody> post(Class<ResultBody> resultClass,
+                                                                                RequestBody requestBody, String endpoint) throws Exception {
+        String json = OBJECT_MAPPER.writeValueAsString(requestBody);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_BASE_URL + endpoint))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+        return executeRequest(resultClass, request);
+    }
+
+    protected static <ResultBody> ApiHttpResponse<ResultBody> get(Class<ResultBody> resultClass, String endpoint) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_BASE_URL + endpoint))
+                .GET().build();
+        return executeRequest(resultClass, request);
+    }
+
+    protected static ApiHttpResponse<?> delete(String endpoint) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_BASE_URL + endpoint))
+                .DELETE().build();
+        return executeRequest(request);
+    }
+
+    private static <ResultBody> ApiHttpResponse<ResultBody> executeRequest(Class<ResultBody> resultClass, HttpRequest request) throws Exception {
+        HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        ResultBody result = OBJECT_MAPPER.readValue(response.body(), resultClass);
+        return new ApiHttpResponse<>(response, result);
+    }
+
+    private static ApiHttpResponse<?> executeRequest(HttpRequest request) throws Exception {
+        HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        return new ApiHttpResponse<>(response, Void.class);
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    protected static class ApiHttpResponse<ResultBody> {
+        private final HttpResponse<String> httpResponse;
+        private final ResultBody resultBody;
     }
 }
--- server/api/src/test/java/com/twb/pokergame/service/game/impl/TexasHoldemGameIT.java
+++ server/api/src/test/java/com/twb/pokergame/service/game/impl/TexasHoldemGameIT.java
@@ -2,13 +2,18 @@ package com.twb.pokergame.service.game.impl;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class TexasHoldemGameIT extends BaseTestContainersIT {
 
     @Test
-    public void testActiveProfile() {
-        System.out.println("TexasHoldemGameIT.testActiveProfile");
-        assertTrue(true);
+    public void testActuator() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_BASE_URL + "/actuator"))
+                .GET().build();
+        HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("response = " + response.body());
     }
 }
