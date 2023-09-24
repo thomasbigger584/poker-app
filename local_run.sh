39e5d3ea094b96248d034c4b021b03db394e63b5
Bigger, Thomas <thomasbigger584@gmail.com>
2023-09-24 16:19:37 +0100

    Removing digitalocean files from project

--- local_run.sh
+++ local_run.sh
@@ -2,7 +2,7 @@
 trap ctrl_c INT # trap ctrl-c and call ctrl_c()
 
 function ctrl_c() {
-  docker compose -f server/api/docker-compose.yml rm --force
+  docker compose -f api/docker-compose.yml rm --force
 }
 
-docker compose -f server/api/docker-compose.yml up --build --remove-orphans
+docker compose -f api/docker-compose.yml up --build --remove-orphans
--- remote_run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git checkout master
-
-git pull origin master
-git submodule update --recursive --remote
-
-docker compose -f server/digitalocean/docker-compose.yml up --build --remove-orphans -d
