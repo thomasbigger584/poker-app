0db0a3907200ab0f3520cb2237cce19108e2c099
Thomas Bigger <thomasbigger584@gmail.com>
2024-05-07 12:17:36 +0100

    constant tidyups

--- api/src/test/java/com/twb/pokerapp/utils/testcontainers/BaseTestContainersIT.java
+++ api/src/test/java/com/twb/pokerapp/utils/testcontainers/BaseTestContainersIT.java
@@ -21,48 +21,59 @@ public abstract class BaseTestContainersIT {
     // Keycloak Constants
     private static final String KEYCLOAK_SERVICE = "keycloak";
     private static final String KEYCLOAK_REALM_JSON_FILE_PATH = "/keycloak-realm.json";
+    private static final String KEYCLOAK_HOSTNAME_URL_KEY = "KC_HOSTNAME_URL";
     private static final int KEYCLOAK_PORT = 8080;
+    private static final String KEYCLOAK_SERVER_URL = String.format("http://%s:%d", KEYCLOAK_SERVICE, KEYCLOAK_PORT);
 
     // DB Constants
+    private static final String DB_IMAGE_NAME = "postgres";
+    private static final String DB_IMAGE_VERSION = "13.1-alpine";
     private static final String DB_SERVICE = "postgres";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "password";
+    private static final String DB_NAME = "db";
     private static final int DB_PORT = 5432;
 
     // API Constants
+    private static final String API_IMAGE_NAME = "com.twb.pokerapp/api";
+    private static final String API_IMAGE_VERSION = "latest";
     private static final String API_SERVICE = "api";
+    private static final String KEYCLOACK_SERVER_URL_KEY = "KEYCLOAK_SERVER_URL";
     private static final int API_PORT = 8081;
     private static final int API_DEBUG_PORT = 5005;
 
     // Test Containers
     private static final Network NETWORK = Network.newNetwork();
-    private static final KeycloakContainer KEYCLOAK_CONTAINER = new KeycloakContainer()
-            .withRealmImportFile(KEYCLOAK_REALM_JSON_FILE_PATH)
-            .withAdminUsername(KeycloakClients.KEYCLOAK_ADMIN_USERNAME)
-            .withAdminPassword(KeycloakClients.KEYCLOAK_ADMIN_PASSWORD)
-            .withNetwork(NETWORK)
-            .withNetworkAliases(KEYCLOAK_SERVICE)
-            .withNetworkMode("bridge")
-            .withEnv("KC_HOSTNAME_URL", String.format("http://%s:%d", KEYCLOAK_SERVICE, KEYCLOAK_PORT))
-            .withVerboseOutput();
-    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:13.1-alpine")
-            .withUsername(DB_USERNAME)
-            .withPassword(DB_PASSWORD)
-            .withDatabaseName("db")
-            .withExposedPorts(DB_PORT)
-            .withLogConsumer(new Slf4jLogConsumer(logger).withPrefix(DB_SERVICE))
-            .withNetwork(NETWORK)
-            .withNetworkAliases(DB_SERVICE)
-            .withNetworkMode("bridge")
-            .dependsOn(KEYCLOAK_CONTAINER);
-    private static final GenericContainer<?> API_CONTAINER = new GenericContainer<>("com.twb.pokerapp/api:latest")
-            .withEnv("KEYCLOAK_SERVER_URL", String.format("http://%s:%d", KEYCLOAK_SERVICE, KEYCLOAK_PORT))
-            .withExposedPorts(API_PORT)
-            .withLogConsumer(new Slf4jLogConsumer(logger).withPrefix(API_SERVICE))
-            .withNetwork(NETWORK)
-            .withNetworkAliases(API_SERVICE)
-            .withNetworkMode("bridge")
-            .dependsOn(KEYCLOAK_CONTAINER, POSTGRESQL_CONTAINER);
+    private static final KeycloakContainer KEYCLOAK_CONTAINER =
+            new KeycloakContainer()
+                    .withRealmImportFile(KEYCLOAK_REALM_JSON_FILE_PATH)
+                    .withAdminUsername(KeycloakClients.KEYCLOAK_ADMIN_USERNAME)
+                    .withAdminPassword(KeycloakClients.KEYCLOAK_ADMIN_PASSWORD)
+                    .withNetwork(NETWORK)
+                    .withNetworkAliases(KEYCLOAK_SERVICE)
+                    .withNetworkMode("bridge")
+                    .withEnv(KEYCLOAK_HOSTNAME_URL_KEY, KEYCLOAK_SERVER_URL)
+                    .withVerboseOutput();
+    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER =
+            new PostgreSQLContainer<>(DB_IMAGE_NAME + ":" + DB_IMAGE_VERSION)
+                    .withUsername(DB_USERNAME)
+                    .withPassword(DB_PASSWORD)
+                    .withDatabaseName(DB_NAME)
+                    .withExposedPorts(DB_PORT)
+                    .withLogConsumer(new Slf4jLogConsumer(logger).withPrefix(DB_SERVICE))
+                    .withNetwork(NETWORK)
+                    .withNetworkAliases(DB_SERVICE)
+                    .withNetworkMode("bridge")
+                    .dependsOn(KEYCLOAK_CONTAINER);
+    private static final GenericContainer<?> API_CONTAINER =
+            new GenericContainer<>(API_IMAGE_NAME + ":" + API_IMAGE_VERSION)
+                    .withEnv(KEYCLOACK_SERVER_URL_KEY, KEYCLOAK_SERVER_URL)
+                    .withExposedPorts(API_PORT)
+                    .withLogConsumer(new Slf4jLogConsumer(logger).withPrefix(API_SERVICE))
+                    .withNetwork(NETWORK)
+                    .withNetworkAliases(API_SERVICE)
+                    .withNetworkMode("bridge")
+                    .dependsOn(KEYCLOAK_CONTAINER, POSTGRESQL_CONTAINER);
 
     protected static KeycloakClients keycloakClients;
 
