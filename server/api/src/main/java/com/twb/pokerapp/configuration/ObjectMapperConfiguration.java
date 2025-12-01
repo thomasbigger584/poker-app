ff2ffd4971b3bc5426dfbd67931a57a6db28cba0
thomasbigger584 <thomasbigger584@gmail.com>
2025-12-01 22:16:13 +0000

    upgrade to spring 4.0.0 with fixes

--- server/api/src/main/java/com/twb/pokerapp/configuration/ObjectMapperConfiguration.java
+++ server/api/src/main/java/com/twb/pokerapp/configuration/ObjectMapperConfiguration.java
@@ -1,7 +1,8 @@
 package com.twb.pokerapp.configuration;
 
-import tools.jackson.databind.ObjectMapper;
-import tools.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,9 @@ public class ObjectMapperConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         return mapper;
     }
 }
