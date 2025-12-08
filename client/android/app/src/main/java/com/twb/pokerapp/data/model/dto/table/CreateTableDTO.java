a2885645141aa7abb045fa6120a26ce469cbded5
thomasbigger584 <thomasbigger584@gmail.com>
2025-12-08 23:40:36 +0000

    adding additional fields to table

--- client/android/app/src/main/java/com/twb/pokerapp/data/model/dto/table/CreateTableDTO.java
+++ client/android/app/src/main/java/com/twb/pokerapp/data/model/dto/table/CreateTableDTO.java
@@ -1,4 +1,16 @@
 package com.twb.pokerapp.data.model.dto.table;
 
 public class CreateTableDTO {
+
+    private String name;
+
+    private String gameType;
+
+    private Integer minPlayers;
+
+    private Integer maxPlayers;
+
+    private Double minBuyin;
+
+    private Double maxBuyin;
 }
--- client/android/app/src/main/java/com/twb/pokerapp/data/model/dto/table/TableDTO.java
+++ client/android/app/src/main/java/com/twb/pokerapp/data/model/dto/table/TableDTO.java
@@ -11,16 +11,29 @@ public class TableDTO {
     private static final String KEY_TABLE_ID = "table_id";
     private static final String KEY_TABLE_NAME = "table_name";
     private static final String KEY_GAME_TYPE = "game_type";
+    private static final String KEY_MIN_PLAYERS = "min_players";
+    private static final String KEY_MAX_PLAYERS = "max_players";
+    private static final String KEY_MIN_BUYIN = "min_buyin";
+    private static final String KEY_MAX_BUYIN = "max_buyin";
 
     private UUID id;
     private String name;
     private String gameType;
+    private Integer minPlayers;
+    private Integer maxPlayers;
+    private Double minBuyin;
+    private Double maxBuyin;
 
     public static TableDTO fromBundle(Bundle bundle) {
         TableDTO table = new TableDTO();
         table.setId(UUID.fromString(bundle.getString(KEY_TABLE_ID)));
         table.setName(bundle.getString(KEY_TABLE_NAME));
         table.setGameType(bundle.getString(KEY_GAME_TYPE));
+        table.setMinPlayers(bundle.getInt(KEY_MIN_PLAYERS));
+        table.setMaxPlayers(bundle.getInt(KEY_MAX_PLAYERS));
+        table.setMinBuyin(bundle.getDouble(KEY_MIN_BUYIN));
+        table.setMaxBuyin(bundle.getDouble(KEY_MAX_BUYIN));
+
         return table;
     }
 
@@ -29,6 +42,10 @@ public class TableDTO {
         bundle.putString(KEY_TABLE_ID, id.toString());
         bundle.putString(KEY_TABLE_NAME, name);
         bundle.putString(KEY_GAME_TYPE, gameType);
+        bundle.putInt(KEY_MIN_PLAYERS, minPlayers);
+        bundle.putInt(KEY_MAX_PLAYERS, maxPlayers);
+        bundle.putDouble(KEY_MIN_BUYIN, minBuyin);
+        bundle.putDouble(KEY_MAX_BUYIN, maxBuyin);
         return bundle;
     }
 
@@ -56,6 +73,38 @@ public class TableDTO {
         this.gameType = gameType;
     }
 
+    public Integer getMinPlayers() {
+        return minPlayers;
+    }
+
+    public void setMinPlayers(Integer minPlayers) {
+        this.minPlayers = minPlayers;
+    }
+
+    public Integer getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(Integer maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public Double getMinBuyin() {
+        return minBuyin;
+    }
+
+    public void setMinBuyin(Double minBuyin) {
+        this.minBuyin = minBuyin;
+    }
+
+    public Double getMaxBuyin() {
+        return maxBuyin;
+    }
+
+    public void setMaxBuyin(Double maxBuyin) {
+        this.maxBuyin = maxBuyin;
+    }
+
     @NonNull
     @Override
     public String toString() {
@@ -63,6 +112,10 @@ public class TableDTO {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", gameType='" + gameType + '\'' +
+                ", minPlayers=" + minPlayers +
+                ", maxPlayers=" + maxPlayers +
+                ", minBuyin=" + minBuyin +
+                ", maxBuyin=" + maxBuyin +
                 '}';
     }
 }
