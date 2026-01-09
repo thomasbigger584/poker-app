739ed1f0bc9602c5cbba2b6f1def821cfed736c0
thomasbigger584 <thomasbigger584@gmail.com>
2026-01-09 17:25:29 +0000

    docker up and down clean now working in terraform

--- server/terraform/main.tf
+++ server/terraform/main.tf
@@ -3,26 +3,33 @@ variable "project_name" {
   default = "poker-app"
 }
 
+variable "tailnet_domain" {
+  type    = string
+  default = "taila8b6c7.ts.net"
+}
+
 resource "local_file" "nginx_default_conf" {
   content = templatefile("${path.module}/../nginx/conf.d/default.conf.tpl", {
-    server_name = "${var.project_name}.taila8b6c7.ts.net"
+    server_name = "${var.project_name}.${var.tailnet_domain}"
   })
   filename = "${path.module}/../nginx/conf.d/tfgen_default.conf"
 }
 
-resource "null_resource" "docker_compose_up" {
+resource "null_resource" "docker_up" {
   triggers = {
     nginx_conf_content = local_file.nginx_default_conf.content
   }
 
   provisioner "local-exec" {
     working_dir = "${path.module}/.."
-    command     = "docker compose up"
+    command     = "docker compose up --build -d"
   }
+}
 
+resource "null_resource" "docker_down_clean" {
   provisioner "local-exec" {
     when        = destroy
     working_dir = "${path.module}/.."
-    command     = "docker compose down"
+    command     = "docker compose down --remove-orphans; docker volume rm $(docker volume ls -q); docker system prune -a -f"
   }
 }
