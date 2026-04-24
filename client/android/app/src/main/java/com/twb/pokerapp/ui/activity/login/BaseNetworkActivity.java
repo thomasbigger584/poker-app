55bf7779787ebbf40f162ae87eb9bc2c26ad6384
thomasbigger584 <thomasbigger584@gmail.com>
2026-04-24 11:21:26 +0100

    Implement network connectivity monitoring and warning dialog in `BaseNetworkActivity` using `ConnectivityManager` callbacks.

--- client/android/app/src/main/java/com/twb/pokerapp/ui/activity/login/BaseNetworkActivity.java
+++ client/android/app/src/main/java/com/twb/pokerapp/ui/activity/login/BaseNetworkActivity.java
@@ -2,11 +2,16 @@ package com.twb.pokerapp.ui.activity.login;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +27,7 @@ import javax.inject.Inject;
 public abstract class BaseNetworkActivity extends AppCompatActivity {
     private static final String TAG = BaseNetworkActivity.class.getSimpleName();
     private static final String TAILSCALE_DIALOG_TAG = "tailscale_warning_dialog";
+    private static final String NETWORK_DIALOG_TAG = "network_warning_dialog";
 
     @Inject
     public TailscaleController tailscaleController;
@@ -29,7 +35,12 @@ public abstract class BaseNetworkActivity extends AppCompatActivity {
     @Inject
     public AuthConfiguration authConfiguration;
 
+    @Inject
+    public ConnectivityManager connectivityManager;
+
     protected ExecutorService networkExecutor;
+
+    private ConnectivityManager.NetworkCallback networkCallback;
 
     @Override
     @CallSuper
@@ -40,9 +51,30 @@ public abstract class BaseNetworkActivity extends AppCompatActivity {
 
     @Override
     @CallSuper
+    protected void onStart() {
+        super.onStart();
+        registerNetworkCallback();
+        if (!isNetworkAvailable()) {
+            showNetworkWarning();
+        }
+    }
+
+    @Override
+    @CallSuper
     protected void onResume() {
         super.onResume();
-        checkTailscale();
+        if (!isNetworkAvailable()) {
+            showNetworkWarning();
+        } else {
+            checkTailscale();
+        }
+    }
+
+    @Override
+    @CallSuper
+    protected void onStop() {
+        unregisterNetworkCallback();
+        super.onStop();
     }
 
     protected void checkTailscale() {
@@ -50,7 +82,7 @@ public abstract class BaseNetworkActivity extends AppCompatActivity {
             var isConnected = tailscaleController.isTailscaleConnected();
             runOnUiThread(() -> {
                 if (isFinishing() || isDestroyed()) {
-                    Log.i(TAG, "checkTailscale: Finishing or destroying");;
+                    Log.i(TAG, "checkTailscale: Finishing or destroying");
                     return;
                 }
                 if (isConnected) {
@@ -80,7 +112,76 @@ public abstract class BaseNetworkActivity extends AppCompatActivity {
         }
     }
 
+    private void registerNetworkCallback() {
+        if (networkCallback != null) return;
+        networkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                super.onAvailable(network);
+                runOnUiThread(() -> dismissNetworkWarning());
+            }
+
+            @Override
+            public void onLost(@NonNull Network network) {
+                super.onLost(network);
+                runOnUiThread(() -> showNetworkWarning());
+            }
+        };
+        connectivityManager.registerDefaultNetworkCallback(networkCallback);
+    }
+
+    private void unregisterNetworkCallback() {
+        if (networkCallback != null) {
+            connectivityManager.unregisterNetworkCallback(networkCallback);
+            networkCallback = null;
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        var activeNetwork = connectivityManager.getActiveNetwork();
+        if (activeNetwork == null) return false;
+        var capabilities = connectivityManager.getNetworkCapabilities(activeNetwork);
+        return capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+    }
+
+    private void showNetworkWarning() {
+        if (isFinishing() || isDestroyed()) {
+            return;
+        }
+        dismissTailscaleWarning();
+        var fragment = getSupportFragmentManager().findFragmentByTag(NETWORK_DIALOG_TAG);
+        if (fragment != null) {
+            return;
+        }
+        var subtitle = "You are not connected to the internet. Please enable in your network settings.";
+        var dialog = AlertModalDialog.newInstance(AlertModalDialog.AlertModalType.WARNING, subtitle, new AlertModalDialog.OnAlertClickListener() {
+            @Override
+            public void onSuccessClick() {
+                startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+            }
+
+            @Override
+            public void onCancelClick() {
+            }
+        });
+        dialog.show(getSupportFragmentManager(), NETWORK_DIALOG_TAG);
+    }
+
+    private void dismissNetworkWarning() {
+        if (isFinishing() || isDestroyed()) {
+            return;
+        }
+        var fragment = getSupportFragmentManager().findFragmentByTag(NETWORK_DIALOG_TAG);
+        if (fragment instanceof AlertModalDialog) {
+            ((AlertModalDialog) fragment).dismissAllowingStateLoss();
+            checkTailscale();
+        }
+    }
+
     private void showTailscaleWarning() {
+        if (getSupportFragmentManager().findFragmentByTag(NETWORK_DIALOG_TAG) != null) {
+            return;
+        }
         var fragment = getSupportFragmentManager().findFragmentByTag(TAILSCALE_DIALOG_TAG);
         if (fragment != null) {
             return;
