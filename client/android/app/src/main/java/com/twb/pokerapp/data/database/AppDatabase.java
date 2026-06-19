e24152f599c83b61c00bcdf04c1e0b38f2ca2b10
thomasbigger584 <thomasbigger584@gmail.com>
2026-06-19 23:31:51 +0100

    work towards protobuf integration

--- client/android/app/src/main/java/com/twb/pokerapp/data/database/AppDatabase.java
+++ client/android/app/src/main/java/com/twb/pokerapp/data/database/AppDatabase.java
@@ -8,7 +8,7 @@ import com.twb.pokerapp.data.database.converter.UUIDConverter;
 import com.twb.pokerapp.data.database.dao.ServerMessageDAO;
 import com.twb.pokerapp.data.database.entities.ServerMessageEntity;
 
-@Database(entities = {ServerMessageEntity.class}, version = 2, exportSchema = false)
+@Database(entities = {ServerMessageEntity.class}, version = 1, exportSchema = false)
 @TypeConverters({UUIDConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract ServerMessageDAO serverMessageDao();
